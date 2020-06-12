06071699a32a6dedd27113dc02ba66439ab386c8
David Martin <davidmartin1737@gmail.com>
2020-06-12 14:36:13 -0400

    Created models

--- Monitor/models.py
+++ Monitor/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class MonitoredInterface(models.Model):
+    external_id = models.UUIDField(
+        primary_key = True,
+        default = uuid.uuid4,
+        editable = False)
+    mac_address = models.CharField(max_length=12)
+    ipv4_address = models.CharField(max_length=200)
+    interface_name = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+class BadMacAddresses(models.Model):
+    external_id = models.UUIDField(
+        primary_key = True,
+        default = uuid.uuid4,
+        editable = False)
+    mac_address = models.CharField(max_length=200)
+    ipv4_address = models.CharField(max_length=200)
+    interface_name = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)    
--- django_exercise/settings.py
+++ django_exercise/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split()
 # Application definition
 
 INSTALLED_APPS = [
+    'Monitor',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
